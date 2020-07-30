62dce2c1e193a932762d583fed679a9cd5b89a0f
theadityat24 <52205250+theadityat24@users.noreply.github.com>
2020-07-30 16:30:54 -0500

    Added models to notes

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=400)
+
+class Course(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    name = models.CharField(max_length=400)
+    description = models.TextField(max_length=1000)
+    date_started = models.DateField()
+    date_ended = models.DateField(blank=True)
+
+class Lecture(models.Model):
+    date_given = models.DateField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    name = models.CharField(max_length=300)
+
+# TODO: Add User field to all of the models.
