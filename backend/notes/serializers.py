c9bb83aa948348354592b513f7f100c7d3983000
theadityat24 <52205250+theadityat24@users.noreply.github.com>
2020-07-31 11:44:32 -0500

    Added serializers

---
+++ backend/notes/serializers.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import User
+from .models import (
+    School,
+    Course,
+    Lecture,
+    LectureVersion,
+)
+from rest_framework import serializers
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+class SchoolSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = School
+        fields = ['name', 'user']
+
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Course
+        fields = [
+            'school', 'user', 'name',
+            'description', 'date_started',
+            'date_ended'
+        ]
+
+class LectureSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Lecture
+        fields = [
+            'date_given', 'date_added', 'course',
+            'user', 'name', 'note_file'
+        ]
+
+class LectureVersion(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = LectureVersion
+        fields = [
+            'predec', 'date_changed',
+            'user', 'note_file',
+        ]
