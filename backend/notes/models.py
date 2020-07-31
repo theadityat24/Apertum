e25a8fd0355ef9048dc0093c9dd33a585742d9a9
theadityat24 <52205250+theadityat24@users.noreply.github.com>
2020-07-31 15:17:45 -0500

    Added str methods to models

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -5,6 +5,9 @@ class School(models.Model):
     name = models.CharField(max_length=400)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
+    def __str__(self):
+        return self.name
+
 class Course(models.Model):
     school = models.ForeignKey(School, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -12,6 +15,9 @@ class Course(models.Model):
     description = models.TextField(max_length=1000)
     date_started = models.DateField()
     date_ended = models.DateField(blank=True)
+
+    def __str__(self):
+        return self.name
 
 class Lecture(models.Model):
     date_given = models.DateField()
@@ -21,8 +27,15 @@ class Lecture(models.Model):
     name = models.CharField(max_length=300)
     note_file = models.FileField(upload_to='notes')
 
+    def __str__(self):
+        return self.name
+
 class LectureVersion(models.Model):
     predec = models.ForeignKey(Lecture, on_delete=models.CASCADE)
     date_changed = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     note_file = models.FileField(upload_to='notes')
+
+    def __str__(self):
+        return f'{list(Lecture.objects.filter(pk=self.predec))[0]} - {self.date_changed}'
+
