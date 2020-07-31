f9f65952e3025208d9ac6a91139afe2bf30c8b77
theadityat24 <52205250+theadityat24@users.noreply.github.com>
2020-07-31 10:57:18 -0500

    Registered models with admin site

--- backend/notes/admin.py
+++ backend/notes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (
+    School,
+    Course,
+    Lecture,
+)
 
-# Register your models here.
+admin.site.register(School)
+admin.site.register(Course)
+admin.site.register(Lecture)
