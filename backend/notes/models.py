600d150b5705462330780758e694450c4b49cf33
theadityat24 <52205250+theadityat24@users.noreply.github.com>
2020-07-31 15:05:30 -0500

    Added null to all User fields

--- backend/notes/models.py
+++ backend/notes/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 
 class School(models.Model):
     name = models.CharField(max_length=400)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
 class Course(models.Model):
     school = models.ForeignKey(School, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=400)
     description = models.TextField(max_length=1000)
     date_started = models.DateField()
@@ -17,12 +17,12 @@ class Lecture(models.Model):
     date_given = models.DateField()
     date_added = models.DateTimeField(auto_now=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=300)
     note_file = models.FileField(upload_to='notes')
 
 class LectureVersion(models.Model):
     predec = models.ForeignKey(Lecture, on_delete=models.CASCADE)
     date_changed = models.DateTimeField(auto_now=True)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     note_file = models.FileField(upload_to='notes')
