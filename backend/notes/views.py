acd720fc189ba7f7a9d6d5f2acc2e3cf4236c1a1
theadityat24 <52205250+theadityat24@users.noreply.github.com>
2020-07-31 11:56:02 -0500

    Added ListUsers and ListSchools views

--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
+from .models import (
+    School,
+    Course,
+    Lecture,
+    LectureVersion,
+)
 
-# Create your views here.
+class ListUsers(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAdminUser]
+
+    def get(self, request, format=None):
+        usernames = [user.username for user in User.objects.all()]
+        return Response(usernames)
+
+class ListSchools(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, format=None):
+        schools = [s.name for s in School.objects.all()]
+        return Response(schools)
+
